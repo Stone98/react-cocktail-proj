e4b3bbcc3330a1a936e51167483f375fc7fcac22
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2020-12-18 17:33:46 -0700

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta ut magnam amet nostrum ab dolorum minus, dolorem soluta, ea beatae nesciunt dolor ratione iste distinctio, iure totam magni quo atque saepe nobis quae dolore. A hic veritatis obcaecati temporibus quidem?
+      </p>
+    </section>
+  )
+}
+
+export default About
